65ec7dc8fbd542da60e41b2cffe0bf697740c2ee
Stuckens <kelian2009@hotmail.fr>
2022-02-04 14:24:44 +0100

    Ajout du Formulaire de Connexion

---
+++ src/login/login.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+</head>
+
+<body class="bg-primary">
+    <div id="container-xl" class="d-flex justify-content-center py-4">
+        <!-- zone de connexion -->
+        <form action="verification.php" action="POST" class="col-12 col-sm-10 col-md-8 col-lg-2 bg-warning text-dark p-4 rounded shadow">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php
+        if (isset($_GET['erreur'])) {
+            $err = $_GET['erreur'];
+            if ($err == 1 || $err == 2)
+                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+        ?>
+        </form>
+    </div>
+</body>
+
+</html>

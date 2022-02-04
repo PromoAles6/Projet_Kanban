f0fa90ba8bbdc5a1cd582a2fac83aa1718640dfe
stephanie <stephanie.menu.cda@gmail.com>
2022-02-04 16:46:23 +0100

    page board

--- src/Database.php
+++ src/Database.php
@@ -13,7 +13,7 @@ class Database
         try {
             $db = new PDO('mysql:host=' . $config["DB_HOST"] . ';dbname=' . $config["DB_NAME"], $config["DB_USERNAME"], $config["DB_PASSWORD"]);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo '<div>Connexion reussie</div>';
+            //echo '<div>Connexion reussie</div>';
         } catch (\PDOException $e) {
             echo 'Échec lors de la connexion : ' . $e->getMessage();
         }

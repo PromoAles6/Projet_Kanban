62e32ca829c5f023aae6c37fe034ba8ab0534dda
stephanie <stephanie.menu.cda@gmail.com>
2022-02-04 13:48:32 +0100

    modification

--- src/Database.php
+++ src/Database.php
@@ -8,7 +8,8 @@ class Database
 {
     public function Connect()
     {
-        $config = parse_ini_file("config.dist.ini", true);
+        //on recupere les identifiants de la base de donnéées
+        $config = parse_ini_file("config.ini", true);
         try {
             $db = new PDO('mysql:host=' . $config["DB_HOST"] . ';dbname=' . $config["DB_NAME"], $config["DB_USERNAME"], $config["DB_PASSWORD"]);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

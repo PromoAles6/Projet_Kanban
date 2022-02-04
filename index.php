c98c0d5a1ec156f9ccb5176964d7c7e8313f6ff2
Stuckens <kelian2009@hotmail.fr>
2022-02-04 12:12:13 +0100

    Ajout du fichier de Connexion

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+
+use App\database\Database;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 $bonjour= ["Bonjour","Coucou", "Hola"];
-dump($bonjour);
 
-use App\Test;
-$Test = new Test;
-$Test->test();
+
+$connection = new Database;
+$pdo = $connection->getPDO();
+dump($pdo);

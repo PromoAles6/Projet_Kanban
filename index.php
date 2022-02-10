0fe605ffb237d6fe4679db391028f015a574964d
mbouridev <mbouri.dev@gmail.com>
2022-02-10 12:01:24 +0100

    routeur

--- index.php
+++ index.php
@@ -7,5 +7,43 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 
-$connection = new Database;
-$pdo = $connection->getPDO();
+/*$connection = new Database;
+$pdo = $connection->getPDO();*/
+
+class Application
+{
+    const AUTHORIZED_PAGES = [
+        'projets' => 'board',
+        'modal' => 'modal',
+        'inscription' => 'register',
+        'tableau' => 'tableau/index'
+    ];
+
+
+    public function match($route_name)
+    {
+        if (isset(self::AUTHORIZED_PAGES[$route_name]))
+        {
+            $route_name = self::AUTHORIZED_PAGES[$route_name];
+        }
+        else
+        {
+            $route_name = '404';
+        }
+
+        return $route_name;
+    }
+
+
+    public function run()
+    {
+        $route_name = $_GET['page'] ?? 'board';
+        $route_name = $this->match($route_name);
+
+        include_once($route_name.'.php');
+    }
+}
+
+
+$application = new Application();
+$application->run();

0c8a565c506f53eae58b33effbefe531e0f52d9b
johan030 <weylin030@protonmail.ch>
2022-02-10 11:55:17 +0100

    routeur

--- index.php
+++ index.php
@@ -7,5 +7,49 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 
-$connection = new Database;
-$pdo = $connection->getPDO();
+
+
+class Application {
+
+
+        const AUTHORIZED_PAGES = [
+            'tableaux' => 'board',
+            'modal' => 'modal',
+            'inscription' => 'register',
+            'tableau' => 'tableau/index',
+            'board' => 'tableau/index'
+        ];
+
+        private function match($route_name){
+
+                  // je vérifie si la clef existe dans la liste des pages autorisées
+                  if (isset(self::AUTHORIZED_PAGES[$route_name])) {
+                    $route_name = self::AUTHORIZED_PAGES[$route_name];
+                }else{
+                    $route_name = '404';
+                }
+                return $route_name;
+        }
+
+
+        public function run(){
+            //je récupère la route demandée dans l'url
+            //si la page n'est pas spécifiée ( ex: on arrive pour la première fois sur le site)
+            //on redirige vers la page d'accueil
+
+            $route_name = $_GET['page'] ?? 'projets';
+            $route_name = $this->match($route_name);
+            //liste des pages autorisées
+        
+
+      
+
+
+
+            include_once $route_name . '.php';
+
+        }
+
+}
+$application = new Application();
+$application ->run();
--- tableau/index.php
+++ tableau/index.php
@@ -12,7 +12,7 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <link href="style.css" rel="stylesheet" />
+    <link href="tableau/style.css" rel="stylesheet" />
     <title>Hello, world!</title>
   </head>
   <body>

eec31707419ef7b2ccf7b48bb4d7adbf690c6971
Didi518 <kevin.ouali@gmail.com>
2022-02-10 11:55:24 +0100

    router

--- index.php
+++ index.php
@@ -7,5 +7,40 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 
-$connection = new Database;
-$pdo = $connection->getPDO();
+class Application
+{
+    const AUTHORIZED_PAGES = [
+            'projets' => 'board',
+            'modal' => 'modal',
+            'inscription' => 'register',
+            'tableau' => 'tableau/index',
+            'board' => 'tableau/index'
+        ];
+        
+        //je vérifie si la clef existe dans la liste des pages autorisées
+        private function match($route_name){
+            if(isset(self::AUTHORIZED_PAGES[$route_name])){
+                $route_name = self::AUTHORIZED_PAGES[$route_name];
+            } else {
+                $route_name = '404';
+            }
+            return $route_name;
+        }
+
+    public function run()
+    {
+        //je récupère la route demandée dans l'url
+        //si la page n'est pas spécifiée
+        //on redirige sur la page d'accueil
+        $route_name = $_GET['page'] ?? 'projets';
+
+
+        $route_name = $this->match($route_name);
+            
+        //on inclu le fichier qui correspond à la page demandée
+        include_once $route_name . '.php';
+    }
+}
+
+$application = new Application();
+$application->run();

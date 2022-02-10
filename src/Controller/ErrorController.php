c714fedb38daa4b691d97437860eb13738858f4a
Marine <Marine.sautel30@gmail.com>
2022-02-10 16:48:27 +0100

    Controller

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AbstractController;
+
+class ErrorController extends AbstractController
+{
+    public function error404()
+    {
+        $this->render('errors/404.php');
+
+    }
+
+
+}
---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AbstractController;
+
+class RegisterController extends AbstractController
+{
+    public function index()
+    {
+        //ma logique métier ici
+        // exemple récupérer des données en BDD
+        // traiter des formulaire
+        // vérifier que l'utilisateur a les droits
+        // etc...
+
+        $this->render('register.php');
+    }
+
+
+}

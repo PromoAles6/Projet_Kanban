f31a9224d11ef0bf85a30496360145cfdfc7b635
GregoryLapierre <gregory.lapierre83@gmail.com>
2022-02-10 11:59:02 +0100

    implémentation routeur

--- index.php
+++ index.php
@@ -7,5 +7,38 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 
-$connection = new Database;
-$pdo = $connection->getPDO();
+class Application
+{   
+    const AUTHORIZED_PAGES = [
+        'projets'=>'board',
+        'modal'=>'modal',
+        'inscription'=>'register',
+        'tableau'=>'tableau/index',
+        'board'=>'tableau/index'
+    ];
+
+    const DEFAULT_ROUTE = 'projets';
+
+    private function match($route_name)
+    {
+        if (isset(self::AUTHORIZED_PAGES[$route_name])){
+            $route_name = self::AUTHORIZED_PAGES[$route_name];
+        }
+        else{
+            $route_name = '404';
+        }
+        return $route_name;
+    }
+
+    public function run()
+    {
+        $route_name = $_GET['page'] ?? self::DEFAULT_ROUTE;
+
+        $route_name = $this->match($route_name);
+
+        include_once $route_name . '.php';
+    } 
+}
+
+$application = new Application();
+$application->run();

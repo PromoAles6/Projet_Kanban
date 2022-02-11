7d0fcd2feaec50af40ae7089825fef9005f720ba
MessineB <messinebelarousi@gmail.com>
2022-02-11 11:18:15 +0100

    Commit du modele

---
+++ src/Model/BoardModel.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Model;
+
+use App\database\Database;
+use PDO;
+
+class BoardModel 
+{
+    protected $id;
+
+    protected $name;
+
+    protected $pdo;
+
+    const TABLE_NAME ='board';
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->pdo = $database->getPDO();
+    }
+
+    public function findALL()
+    {
+        $sql = 'SELECT  
+            `id`,
+            `name`
+            FROM ' . self ::TABLE_NAME .'
+            ORDER BY `id` ASC'
+        ;
+
+        $pdoStatement = $this->pdo->query($sql);
+        $result = $pdoStatement->fetchAll(PDO::FETCH_CLASS, self::class);
+        //dump($result);
+        return $result;
+    }
+
+    /**
+     * 
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId()
+    {
+        $this->id = $id;
+
+
+        
+    }
+    /** 
+     * @return self
+    */
+
+    /**
+     * 
+     * Get the value of id
+     */
+    public function getName()
+    {
+        return $this->name;
+
+       
+    }
+        /** 
+         * @return self
+        */
+    public function setName()
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}

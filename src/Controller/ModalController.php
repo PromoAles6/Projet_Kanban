b032e0932f52dcca307c7653def2df89a6165b20
samiryakhlef <samir.yakhlef@gmail.com>
2022-02-14 11:01:29 +0100

    maj model

--- src/Controller/ModalController.php
+++ src/Controller/ModalController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Model\CardModel;
 use App\Controller\AbstractController;
 
 class ModalController extends AbstractController
 {
     public function index()
     {
+        $cardModel = new CardModel();
+
+        $modals = $cardModel->findAll();
         // ma logique métier ici
         // exemple récupérer des données en BDD
         // traiter des formulaire
         // vérifier que l'utilisateur a les droits
         // etc...
+        $this->render('modal.php', [
+            'modals' => $modals
+        ]);
 
         $this->render('modal.php');
     }
--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+
+use App\Model\ListModel;
 use App\Controller\AbstractController;
 
 class TableController extends AbstractController
 {
     public function index()
     {
+        $listModel = new ListModel();
+
+        $lists = $listModel->findAll();
         // ma logique métier ici
         // exemple récupérer des données en BDD
         // traiter des formulaire
         // vérifier que l'utilisateur a les droits
         // etc...
+        dump($lists);
+        $this->render('tableau/index.php', [
+            'lists' => $lists,
+        ]);
 
-        $this->render('tableau/index.php');
     }
 }

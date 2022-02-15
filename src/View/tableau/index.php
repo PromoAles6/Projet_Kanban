f7227ca4ce39251f57ba732641eb75d3d198eb17
samiryakhlef <samir.yakhlef@gmail.com>
2022-02-15 16:20:17 +0100

    create a dynamic  list and card with jquery

--- src/View/tableau/index.php
+++ src/View/tableau/index.php
@@ -9,66 +9,45 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <link href="style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="app.css">
+  
+    <script defer src="js.js"></script>
   <title>liste des cartes</title>
 </head>
 
 <body>
   <div class="container-xl">
     <form method="POST" action="index.php" class="add-list">
-      <input type="text" name="title_list" placeholder="Titre de la liste" class="form-control" id="title_list" aria-describedby="emailHelp" />
+    <div class="oversort">
+      
+    </div>
       <button type="submit" class="btn btn-primary">Ajouter</button>
     </form>
-
-    
-    <div class="container-list">
-      <?php foreach($list as $list): ?>
-        <div class="list">
-          <h2><?= $list->getName() ?></h2>
-          <?php foreach($modals as $card): ?> 
-          <div class="card">
-            <h3><?= $card->getName() ?></h3>
-          </div>
-          <?php endforeach ?>
-          <a href="#" class="btn">+ Ajouter une carte</a>
-        </div>
-      <?php endforeach ?>
-      </div>
+<!-- TEST ADD LISTS  AND CARDS-->
+    <div class="oversort">
+      <input type="text" class="newlistinput" name="newlistname" placeholder="Add New List..." />
     </div>
-  </div>
 
 
 
-      <div class="list">
-
-        <h2>Titre liste</h2>
-        <div class="card">
-          <h3>Titre carte</h3>
-        </div>
-        <div class="card">
-          <h3>Titre carte</h3>
-        </div>
-        <div class="card">
-          <h3>Titre carte</h3>
-        </div>
-        <div class="card">
-          <h3>Titre carte</h3>
-        </div>
-        <a href="#" class="btn">+ Ajouter une carte</a>
-      </div>
-    
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-    </div>
-  </div>
 
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</div>
+</div>
 
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
+
+    <!-- JQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+      </body>
 
 </html>

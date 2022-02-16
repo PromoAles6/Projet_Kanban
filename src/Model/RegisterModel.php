f21a4166ba7897091b6289a5ff35d63c346836ec
GregoryLapierre <gregory.lapierre83@gmail.com>
2022-02-16 14:24:08 +0100

    table_jquery

---
+++ src/Model/RegisterModel.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+use App\database\Database;
+
+class RegisterModel
+{
+    protected $id;
+
+    protected $email;
+
+    protected $password;
+
+    protected $pdo;
+
+    const TABLE_NAME = 'user';
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->pdo = $database->getPDO();
+    }
+
+    public function findAll()
+    {
+        $sql = 'SELECT
+                `id`
+                ,`email`
+                ,`password`
+                FROM ' . self::TABLE_NAME . '
+                ORDER BY `id` ASC;
+        ';
+
+        $pdoStatement = $this->pdo->query($sql);
+        $result = $pdoStatement->fetchAll(PDO::FETCH_CLASS, self::class);
+        return $result;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
--- src/View/tableau/index.php
+++ src/View/tableau/index.php
@@ -1,116 +1,21 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <link href="style.css" rel="stylesheet" />
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container-xl">
-      <form method="POST" action="liste.php" class="add-list">
-        <input
-          type="text"
-          name="title_list"
-          placeholder="Titre de la liste"
-          class="form-control"
-          id="title_list"
-          aria-describedby="emailHelp"
-        />
-        <button type="submit" class="btn btn-primary">Ajouter</button>
-      </form>
-
-      <div class="container-list">
-        <div class="list">
-          <h2>Titre liste</h2>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <a href="#" class="btn">+ Ajouter une carte</a>
-        </div>
-
-        <div class="list">
-          <h2>Titre liste</h2>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <a href="#" class="btn">+ Ajouter une carte</a>
-        </div>
-
-        <div class="list">
-          <h2>Titre liste</h2>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <a href="#" class="btn">+ Ajouter une carte</a>
-        </div>
-
-        <div class="list">
-          <h2>Titre liste</h2>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <div class="card">
-            <h3>Titre carte</h3>
-          </div>
-          <a href="#" class="btn">+ Ajouter une carte</a>
-        </div>
-      </div>
-    </div>
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="style.css" rel="stylesheet" >
+</head>
+<body>
+<div class="oversort">
+    <input type="text" class="newlistinput" name="newlistname" placeholder="Ajouter une nouvelle liste" />
+</div>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+  <script src="src/View/tableau/script.js"></script>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 </html>

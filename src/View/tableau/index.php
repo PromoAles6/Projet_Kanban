8acd03e42073f19afecd0fbe88569b3d7f6ae84f
samiryakhlef <samir.yakhlef@gmail.com>
2022-02-16 15:34:54 +0100

    maj2

--- src/View/tableau/index.php
+++ src/View/tableau/index.php
@@ -11,7 +11,7 @@
   <link href="list.css" rel="stylesheet" />
   <link href="modal.css" rel="stylesheet" />
   <title>Hello, world!</title>
-  
+
 </head>
 
 <body>
@@ -23,117 +23,120 @@
       <button type="submit" class="btn btn-primary" class="addList">Ajouter</button>
     </form>
 
-    <?php foreach ($lists as $list) : ?>
 
-      <div class="container-list">
-
+    <div class="container-list">
+      <!-- Boucle des liste -->
+      <?php foreach ($lists as $list) : ?>
         <div class="list">
 
           <h2><?= $list->getName() ?></h2>
 
-          <div class="card">
-            <button type="button" class="btn" id="addCard" data-bs-toggle="modal" data-bs-target="#list<?=$list->getId()?>" data-bs-whatever="@mdo">Bouton a modifier</button>
-            <?php foreach ($cards as $card) : ?>
-            <div>
-                <?php if ($card->getList_id() === $list->getId()) : ?>
-                  <button type="button" class="btn btn-<?= $card->getColor() ?>" data-bs-toggle="modal" data-bs-target="#modal<?= $card->getId() ?>" data-bs-whatever="@mdo"><?= $card->getName() ?></button>
-                  <?php endif ?>
-                </div>
+          <!-- Boucle des cards -->
+          <?php foreach ($list->cards as $card) : ?>
+            <div class="card">
+              <div>
+                <button type="button" class="btn btn-<?= $card->getColor() ?>" data-bs-toggle="modal" data-bs-target="#modal<?= $card->getId() ?>" data-bs-whatever="@mdo"><?= $card->getName() ?></button>
+              </div>
 
 
-            <!-- Modal with id -->
-            <div class="modal fade" id="modal<?= $card->getId() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  
-                  <div class="modal-header">
-                    <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value="<?= $card->getName() ?> "></input>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-
-                  <div class="modal-body">
-                    <form>
-
-                      <div class="mb-3">
-                        <label for="message-text" class="col-form-label"></label>
-                        <textarea class="form-control" id="message-text" placeholder="rentrez votre description"><?= $card->getContent() ?> </textarea>
-                      </div>
-
-                    </form>
-                  </div>
-
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">modifier</button>
+              <!-- Modal with id -->
+              <!-- <div class="modal fade" id="modal<?= $card->getId() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
                     
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="modal-header">
+                      <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value="<?= $card->getName() ?> "></input>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                      <form>
+
+                        <div class="mb-3">
+                          <label for="message-text" class="col-form-label"></label>
+                          <textarea class="form-control" id="message-text" placeholder="rentrez votre description"><?= $card->getContent() ?> </textarea>
+                        </div>
+
+                      </form>
+                    </div>
+
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">modifier</button>
                       
-                      <div class="btn-group me-2" role="group" aria-label="First group">
-                        <button type="button" class="btn btn-success">vert</button>
-                        <button type="button" class="btn btn-warning">jaune</button>
-                        <button type="button" class="btn btn-danger">rouge</button>
-                        <button type="button" class="btn btn-primary">bleu</button>
+                      <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                        
+                        <div class="btn-group me-2" role="group" aria-label="First group">
+                          <button type="button" class="btn btn-success">vert</button>
+                          <button type="button" class="btn btn-warning">jaune</button>
+                          <button type="button" class="btn btn-danger">rouge</button>
+                          <button type="button" class="btn btn-primary">bleu</button>
+                        </div>
+
                       </div>
 
                     </div>
 
                   </div>
-
                 </div>
-              </div>
-            </div>
+              </div> -->
 
 
 
-            <!-- Modal without id -->
-            <div class="modal fade" id="list<?=$list->getId()?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  
-                  <div class="modal-header">
-                    <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value=""></input>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-
-                  <div class="modal-body">
-                    <form>
-
-                      <div class="mb-3">
-                        <label for="message-text" class="col-form-label"></label>
-                        <textarea class="form-control" id="message-text" placeholder="rentrez votre description"></textarea>
-                      </div>
-
-                    </form>
-                  </div>
-
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">modifier</button>
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                      
-                      <div class="btn-group me-2" role="group" aria-label="First group">
-                        <button type="button" class="btn btn-success">vert</button>
-                        <button type="button" class="btn btn-warning">jaune</button>
-                        <button type="button" class="btn btn-danger">rouge</button>
-                        <button type="button" class="btn btn-primary">bleu</button>
-                      </div>
-
+              <!-- Modal without id -->
+              <!-- <div class="modal fade" id="list<?= $list->getId() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    
+                    <div class="modal-header">
+                      <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value=""></input>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+
+                    <div class="modal-body">
+                      <form>
+
+                        <div class="mb-3">
+                          <label for="message-text" class="col-form-label"></label>
+                          <textarea class="form-control" id="message-text" placeholder="rentrez votre description"></textarea>
+                        </div>
+
+                      </form>
+                    </div>
+
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">modifier</button>
+                      <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                        
+                        <div class="btn-group me-2" role="group" aria-label="First group">
+                          <button type="button" class="btn btn-success">vert</button>
+                          <button type="button" class="btn btn-warning">jaune</button>
+                          <button type="button" class="btn btn-danger">rouge</button>
+                          <button type="button" class="btn btn-primary">bleu</button>
+                        </div>
+
+                      </div>
+                    </div>
+
                   </div>
-
                 </div>
-              </div>
+              </div> -->
+
+
             </div>
-
-
           <?php endforeach ?>
-          </div>
-        </div>
-      </div>
+          <!-- fin boucle cards -->
 
-    <?php endforeach ?>
+          <button type="button" class="btn btn-secondary" id="addCard" data-bs-toggle="modal" data-bs-target="#list<?= $list->getId() ?>" data-bs-whatever="@mdo">Ajouter une carte</button>
+
+        </div>
+      <?php endforeach ?>
+      <!-- fin boucle des listes -->
+    </div>
+
   </div>
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -154,43 +157,43 @@
     })
 
     myModal.addEventListener('shown.bs.modal', function() {
-      myButton.focus()
-      console.log('test')
-    })
-    <script>
-        $(document).ready(function() {
- 
-            $("#submit").click(function() {
- 
-                var name = $("#title_list").val();
-                var board_id = 1;
-                var sort = 1;
- 
-                if(name==''|| board_id =='1'|| sort=='1') {
-                    alert("Please fill all fields.");
-                    return false;
-                }
- 
-                $.ajax({
-                    type: "POST",
-                    url: "store.php",
-                    data: {
-                        name = name,
-                        board_id: board_id,
-                        sort=sort,
-                    },
-                    cache: false,
-                    success: function(data) {
-                        alert(data);
-                    },
-                    error: function(xhr, status, error) {
-                        console.error(xhr);
-                    }
-                });
-                 
-            });
+        myButton.focus()
+        console.log('test')
+      }) <
+      script >
+      $(document).ready(function() {
+
+        $("#submit").click(function() {
+
+          var name = $("#title_list").val();
+          var board_id = 1;
+          var sort = 1;
+
+          if (name == '' || board_id == '1' || sort == '1') {
+            alert("Please fill all fields.");
+            return false;
+          }
+
+          $.ajax({
+            type: "POST",
+            url: "store.php",
+            data: {
+              name = name,
+              board_id: board_id,
+              sort = sort,
+            },
+            cache: false,
+            success: function(data) {
+              alert(data);
+            },
+            error: function(xhr, status, error) {
+              console.error(xhr);
+            }
+          });
+
         });
-    </script>
+      });
+  </script>
   </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>

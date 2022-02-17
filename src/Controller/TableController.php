6c1b460fe4d03aa1da18d92cfda35b03f2417dee
Didi518 <kevin.ouali@gmail.com>
2022-02-17 14:27:40 +0100

    list_ajax

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -48,5 +48,21 @@ class TableController extends AbstractController
             'cards' => $cards
         ]);
     }
-}
 
+    public function create()
+    {
+        // je récupère le name et le list_id depuis le formulaire
+        // soumis en javascript
+        $boardId = $_POST['boardId'];
+        $name = $_POST['name'];
+
+        // je crée une card
+        $listModel= new ListModel();
+        $newListId = $listModel->create($name, $boardId);
+
+        // je renvoie l'id de la card en json
+        $this->sendJson([
+            'id' => $newListId
+        ]);
+    }
+}

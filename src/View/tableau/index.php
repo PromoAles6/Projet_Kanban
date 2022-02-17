8c028d8d430a7f19f2b10e885b188cc97fcffdc2
samiryakhlef <samir.yakhlef@gmail.com>
2022-02-17 10:58:06 +0100

    maj

--- src/View/tableau/index.php
+++ src/View/tableau/index.php
@@ -41,28 +41,9 @@
                     <button type="button" class="btn btn-<?= $card->getColor() ?>" data-bs-toggle="modal" data-bs-target="#modal<?= $card->getId() ?>" data-bs-whatever="@mdo"><?= $card->getName() ?></button>
                 </div>
 
-    <div class="oversort">
-      <input type="text" class="newlistinput" name="newlistname" placeholder="Ajouter une nouvelle liste" />
-    </div>
 
-    
-    <div id="listContainer" class="container-list">
-      <!-- Boucle des liste -->
-        <?php foreach ($lists as $list) : ?>
-          <div class="list">
-            
-            <h2><?= $list->getName() ?></h2>
-            
-            <!-- Boucle des cards -->
-            <?php foreach ($list->cards as $card) : ?>
-              <div class="card">
-                <div>
-                    <button type="button" class="btn btn-<?= $card->getColor() ?>" data-bs-toggle="modal" data-bs-target="#modal<?= $card->getId() ?>" data-bs-whatever="@mdo"><?= $card->getName() ?></button>
-                </div>
-
-
-              <!-- Modal with id -->
-              <!-- <div class="modal fade" id="modal<?= $card->getId() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <!-- Modal with id -->
+                <!-- <div class="modal fade" id="modal<?= $card->getId() ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     
@@ -70,18 +51,14 @@
                       <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value="<?= $card->getName() ?> "></input>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-
                     <div class="modal-body">
                       <form>
-
                         <div class="mb-3">
                           <label for="message-text" class="col-form-label"></label>
                           <textarea class="form-control" id="message-text" placeholder="rentrez votre description"><?= $card->getContent() ?> </textarea>
                         </div>
-
                       </form>
                     </div>
-
                     <div class="modal-footer">
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
@@ -112,18 +89,14 @@
                       <input class="modal-title" id="exampleModalLabel" placeholder="titre carte" value=""></input>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-
                     <div class="modal-body">
                       <form>
-
                         <div class="mb-3">
                           <label for="message-text" class="col-form-label"></label>
                           <textarea class="form-control" id="message-text" placeholder="rentrez votre description"></textarea>
                         </div>
-
                       </form>
                     </div>
-
                     <div class="modal-footer">
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">enregister</button>
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">supprimer</button>
@@ -136,10 +109,8 @@
                           <button type="button" class="btn btn-danger">rouge</button>
                           <button type="button" class="btn btn-primary">bleu</button>
                         </div>
-
                       </div>
                     </div>
-
                   </div>
                 </div>
               </div> -->
@@ -165,16 +136,14 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   
   <!-- jquery ui -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <!-- <script>
     var myModal = document.getElementById('myModal')
     var myInput = document.getElementById('myInput')
     var myButton = document.getElementById('addCard')
-
     myModal.addEventListener('shown.bs.modal', function() {
       myInput.focus()
     })
-
     myModal.addEventListener('shown.bs.modal', function() {
       myButton.focus()
       console.log('test')
@@ -216,116 +185,8 @@
   </script> -->
 
   <!-- script pour le drag and drop -->
-  <script>
+  <script src="assets/js/board.js"></script>
 
-  function updateListSortables(){
-    $( ".sortable" ).sortable({
-      connectWith: ".sortable",
-      items: ":not(.nodrag)",
-      placeholder: "sortable-placeholder ui-corner-all",
-      change: function() {
-          var list = $(this).closest('.sortable');
-          var anchorBottom = $(list).find('.anchorBottom');
-          $(list).append($(anchorBottom).detach());
-        }
-    });
-  }
-
-
-  $(document).ready(function(){
-    updateListSortables();
-    $(".oversort").sortable({items: ":not(.nodrag)", placeholder: "sortable-placeholder" });
-    
-
-
-    // key listener to add new lists
-    // $('input[name="newlistname"]').keyup(function(event){
-    //   if(event.key == "Enter" || event.keyCode == "13"){
-    //     updateListSortables();
-        
-    //     var oversort = $(this).closest('.oversort');
-    //     $( oversort ).scrollLeft( $(oversort).prop("scrollWidth") - $(oversort).width() );
-    //   }
-      
-    // });
-
-    // function pour ajouter une card
-    function addNewCard(event) {
-      // si la touche tapée est entrée (clavier normal ou numérique)
-      if(event.key == "Enter" || event.keyCode == "13"){
-        // j'ajoute une nouvelle card juste avant l'input
-        const cardContent = $(this).val().trim();
-        if (cardContent == "") {
-          return false;
-        }
-
-        // je créé une card
-        const newCard = `
-          <div class="card">
-            <div>
-                <button type="button" class="btn btn-">${cardContent}</button>
-            </div>
-          </div>      
-        `;
-
-        // j'ajoute la card
-        $(this).before(newCard);
-
-        // je vide l'input
-        $(this).val('');
-      }
-    }
-
-    // event pour ajouter une cards
-    $('input[name="newlistitem"]').unbind().keyup(addNewCard);
-
-
-    // ajouter une liste
-    $('#newListForm').submit(function(event){
-      // j'empêche de rechargement de la page
-      event.preventDefault();
-      // je récupère le nom de la liste dans l'input
-      const listName = $("#titleList").val().trim();
-
-      // si le nom de la liste est vide, je ne fais rien
-      if(listName == "")
-      {
-        return false;
-      }
-
-      // je créé un formulaire d'ajout de card
-      $newCardForm = $('<input>')
-      // je set les attribut de mon input
-        .attr({
-            type: 'text',
-            class: 'nodrag anchorBottom newlistitem form-control',
-            name: 'newlistitem',
-            placeholder: 'Ajouter une nouvelle carte'
-        })
-      ;
-
-      // j'ajoute l'event à mon formulaire
-      $($newCardForm).unbind().keyup(addNewCard)
-      
-      // je créé une nouvelle liste
-      $newList = $('<div>')
-        .addClass('list')
-        // j'ajoute le titre de la liste
-        .append('<h2>'+listName+'</h2>')
-        // j'ajoute le formulaire d'ajout de card
-        .append($newCardForm)
-      ;
-
-      // je la rajoute dans la div avec l'id listContainer
-      $("#listContainer").append($newList);
-
-      // je vide l'input
-      $("#titleList").val('');
-    })
-  });   
-
-  </script>
 </body>
 
 </html>
-
